3f4582e17ef94aae17ff54be9b260d7a160b975b
Sponja- <tomas.spognardi@gmail.com>
2023-01-16 00:50:55 -0300

    feat(wrapper): Added config options

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { Logger } from "winston";
 
 type LogStreamWrapperOptions = {
     level: string;
+    splitLines?: boolean;
+    skipEmptyLines?: boolean;
+    lineSeparator?: string;
 };
 
 /**
@@ -11,19 +14,44 @@ type LogStreamWrapperOptions = {
 class LogStreamWrapper extends Writable {
     private logger: Logger;
     private level: string;
+    private splitLines: boolean;
+    private skipEmptyLines: boolean;
+    private lineSeparator: string;
 
-    constructor(logger: Logger, { level }: LogStreamWrapperOptions) {
+    constructor(
+        logger: Logger,
+        {
+            level,
+            splitLines = true,
+            skipEmptyLines = true,
+            lineSeparator = "\n",
+        }: LogStreamWrapperOptions
+    ) {
         super();
+
         this.logger = logger;
+
+        // Config options
         this.level = level;
+        this.splitLines = splitLines;
+        this.skipEmptyLines = skipEmptyLines;
+        this.lineSeparator = lineSeparator;
     }
 
     _write(chunk: any, encoding: BufferEncoding, callback: Function) {
-        const s: string = chunk.toString(encoding);
+        const decoded: string = chunk.toString(encoding);
 
-        const messages = s.split("\n").filter((message) => message.length > 0);
+        if (!this.splitLines) {
+            this.logger.log(this.level, decoded);
+        } else {
+            let messages = decoded.split(this.lineSeparator);
 
-        for (const message of messages) this.logger.log(this.level, message);
+            if (this.skipEmptyLines)
+                messages = messages.filter((message) => message.length > 0);
+
+            for (const message of messages)
+                this.logger.log(this.level, message);
+        }
 
         callback();
     }
